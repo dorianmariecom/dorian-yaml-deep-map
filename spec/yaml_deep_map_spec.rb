81013348d5c9b3d6b25420024240bf74e95ee149
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 11:59:03 +0200

    update yaml-deep-map script and add test

--- spec/yaml_deep_map_spec.rb
+++ spec/yaml_deep_map_spec.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tempfile"
 
-RSpec.describe "each" do
+RSpec.describe "yaml-deep-map" do
+  it "works" do
+    file = Tempfile.create
+    File.write(file, <<~INPUT)
+      - 1
+      - 2
+      - 3
+    INPUT
+    expect(
+      `cat #{file.path} | bin/yaml-deep-map "it += 1 if it.is_a?(Integer)"`
+    ).to eq(<<~OUTPUT)
+      ---
+      - 2
+      - 3
+      - 4
+    OUTPUT
+  end
 end
